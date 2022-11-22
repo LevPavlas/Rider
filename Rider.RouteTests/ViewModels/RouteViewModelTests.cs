1a94e09464e0d6d66c443ff88782c59f5d6dd045
Lev Pavlas <lpavlas@msn.com>
2022-11-22 18:31:58 +0100

    commit 2

--- Rider.RouteTests/ViewModels/RouteViewModelTests.cs
+++ Rider.RouteTests/ViewModels/RouteViewModelTests.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rider.Route.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Rider.RouteTests.ViewModels
+namespace Rider.Route.ViewModels.Tests
 {
-	internal class RouteViewModelTests
+	[TestClass()]
+	public class RouteViewModelTests
 	{
+		[TestMethod()]
+		public void RouteViewModelTest()
+		{
+			Assert.Fail();
+		}
 	}
 }
